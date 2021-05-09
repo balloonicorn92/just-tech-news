5119a39fb9b383cc64b4158549c7b708eb273be7
Fernanda Frers <fernanda.frers@gmail.com>
2021-05-09 00:40:23 -0500

    posts CRUD

--- models/index.js
+++ models/index.js
@@ -1,8 +1,13 @@
 const User = require('./User');
+const Post = require('./Post')
 
+//create associations -- user has many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+// post can belong to only one user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+})
 
-
-
-
-
-module.exports = { User }
+module.exports = { User, Post }
